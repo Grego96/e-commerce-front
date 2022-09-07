8014306582b0ac699f586477bf391fa071f1c79e
Gregory <gregoryhunkeler@gmail.com>
2022-09-07 20:46:31 -0300

    home 40%

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 function FeaturedCard({ bicycleImg }) {
   return (
-    <div className="col-6">
-      <div className="featured-card">
+    <div className="featured-card">
         <img src={bicycleImg} alt="" className="right-img-card" />
-        <h6>Title</h6>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        </p>
-        <button className="featured-card-button">View this product</button>
-      </div>
+      <h6>Title</h6>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+      <button className="featured-card-button">View this product</button>
     </div>
   );
 }
