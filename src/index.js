e9b3f31e7fbf322e9faf05312ba6dce8cd966e44
Tayra Cutri <tayracutri10@gmail.com>
2022-09-22 11:05:12 -0300

    scroll to top actualizado

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
